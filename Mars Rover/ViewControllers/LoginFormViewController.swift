87f222159500254c46a0070960c1f250f7c04cca
h33h <beolki102@gmail.com>
2021-10-31 08:42:14 +0300

    Add login screen to storyboard

---
+++ Mars Rover/ViewControllers/LoginFormViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginFormViewController.swift
+//  Mars Rover
+//
+//  Created by XXX on 31.10.21.
+//
+
+import UIKit
+
+class LoginFormViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
