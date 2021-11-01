c2058f1a9e1e1a1759556a960a8f5e87bd52aafa
h33h <beolki102@gmail.com>
2021-11-01 20:33:25 +0300

    Add realisation of button methods & handling delegate methods of presenter

--- Mars Rover/ViewControllers/MainMenuViewController.swift
+++ Mars Rover/ViewControllers/MainMenuViewController.swift
@@ -6,11 +6,49 @@
 //
 
 import UIKit
-import QuartzCore
-import SceneKit
 
 class MainMenuViewController: UIViewController {
+    private var presenter: MainMenuPresenter?
+    @IBOutlet private var userNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter = MainMenuPresenter()
+        presenter?.setDelegate(delegate: self)
+        presenter?.getProfile()
+    }
+    @IBAction private func logOutAction(_ sender: Any) {
+        presenter?.logout()
+    }
+}
+
+extension MainMenuViewController: MainMenuPresenterDelegate {
+    func logoutAction() {
+        dismiss(animated: true, completion: nil)
+    }
+    func logoutError(error: String) {
+        showSimpleNotificationAlert(title: "Sign out error",
+                                    description: error)
+    }
+    func profileGet(profile: ProfileModel) {
+        DispatchQueue.main.async { [weak self] in
+            self?.userNameLabel.text = "Logged as: \(String(describing: profile.username))"
+        }
+    }
+    func profileGetFailure(error: String) {
+        showSimpleNotificationAlert(title: "Profile Error",
+                                    description: error)
+    }
+}
+
+extension MainMenuViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        FadeAnimationController(animationDuration: 2,
+                                animationType: .dismiss)
+    }
+    func animationController(forPresented presented: UIViewController,
+                             presenting: UIViewController,
+                             source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        FadeAnimationController(animationDuration: 2,
+                                animationType: .present)
     }
 }
