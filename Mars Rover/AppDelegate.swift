c5215067f1a06ae2b8c2e925477a7ae97b3aee9c
h33h <beolki102@gmail.com>
2021-11-02 00:17:58 +0300

    Add spaces between var and func

--- Mars Rover/AppDelegate.swift
+++ Mars Rover/AppDelegate.swift
@@ -10,8 +10,12 @@ import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
     var window: UIWindow?
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Use Firebase library to configure APIs
         FirebaseApp.configure()
         return true
     }
