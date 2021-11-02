75fd7a5be86d81c483b172a2e820e0d5938021b7
h33h <beolki102@gmail.com>
2021-11-02 15:35:06 +0300

    Create LoginFormViewController in ViewControllers folder

---
+++ Mars Rover/ViewControllers/LoginFormViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginFormViewController.swift
+//  Mars Rover
+//
+//  Created by XXX on 2.11.21.
+//
+
+import UIKit
+
+class LoginFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
