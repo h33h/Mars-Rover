f13f75675aa24d7e58a1ec005d9071cfdbf98808
h33h <beolki102@gmail.com>
2021-11-02 16:04:14 +0300

    Add AuthorizationTextField (Custom text field) in Views folder

---
+++ Mars Rover/Views/AuthorizationTextField.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorizationTextField.swift
+//  Mars Rover
+//
+//  Created by XXX on 2.11.21.
+//
+
+import UIKit
+
+class AuthorizationTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
