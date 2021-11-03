964ce3081080a18105fb530495740b2157213914
h33h <beolki102@gmail.com>
2021-11-03 22:02:28 +0300

    Add FirebaseAuthService as union of FirebaseSignInService & FirebaseSignUpService classes

---
+++ Mars Rover/Service/FirebaseAuthService.swift
@@ -0,0 +1,44 @@
+//
+//  FirebaseAuthService.swift
+//  Mars Rover
+//
+//  Created by XXX on 3.11.21.
+//
+
+import Foundation
+import FirebaseAuth
+
+final class FirebaseAuthService {
+
+    // MARK: - FirebaseAuthService: Variables
+    private let auth = Auth.auth()
+    private var signInService = FirebaseSignInService()
+    private var signUpService = FirebaseSignUpService()
+
+    // MARK: - FirebaseAuthService: Methods
+    public func signIn(signInType: SignInType, completion: @escaping SignInCompletion) {
+        switch signInType {
+        case .emailAndPassword(let email, let password):
+            signInService.signInWithPassword(email: email, password: password, completion: completion)
+        case .checkSignIn:
+            signInService.checkSignIn(completion: completion)
+        }
+    }
+
+    public func signUp(signUpType: SignUpType, completion: @escaping SignUpCompletion) {
+        switch signUpType {
+        case .emailAndPassword(let email, let password):
+            signUpService.createAccountWithPassword(email: email, password: password, completion: completion)
+        }
+    }
+
+    public func signOut(errorHandler: @escaping (Error?) -> Void) {
+        do {
+            try auth.signOut()
+            errorHandler(nil)
+        } catch {
+            errorHandler(error)
+        }
+    }
+
+}
