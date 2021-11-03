cb1073bf878c67bd883891658d19da186cef5c1e
h33h <beolki102@gmail.com>
2021-11-03 20:14:02 +0300

    Add FirebaseProfileService to Service folder Add fetchProfileFromFirestore and setupNewProfile methods with realisation

---
+++ Mars Rover/Service/FirebaseProfileService.swift
@@ -0,0 +1,60 @@
+//
+//  FirebaseProfileService.swift
+//  Mars Rover
+//
+//  Created by XXX on 3.11.21.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+public enum ProfileError {
+
+    // MARK: - Enum with Profile error types
+    case error(error: String)
+
+    case profileNotExist
+
+}
+
+typealias ProfileCompletion = (ProfileModel?, ProfileError?) -> Void
+typealias RWProfileCompletion = (Bool, ProfileError?) -> Void
+
+class FirebaseProfileService {
+
+    // MARK: - FirebaseProfileService: Variables
+    private var db = Firestore.firestore()
+
+    // MARK: - FirebaseProfileService: Methods
+    public func fetchProfileFromFirestore(uid: String, completion: @escaping ProfileCompletion) {
+        let userRef = db.collection("users").document(uid)
+        userRef.getDocument { document, error in
+            let result = Result {
+                try document?.data(as: ProfileModel.self)
+            }
+            switch result {
+            case .success(let profileModel):
+                if let profileModel = profileModel {
+                    return completion(profileModel, nil)
+                } else {
+                    return completion(nil, .profileNotExist)
+                }
+            case .failure(let error):
+                return completion(nil, .error(error: error.localizedDescription))
+            }
+        }
+    }
+
+    public func setupNewProfile(uid: String, completion: @escaping RWProfileCompletion) {
+        db.collection("users").document(uid).setData([
+            "username": "Player"
+            ]) { error in
+            if let error = error {
+                return completion(false, .error(error: error.localizedDescription))
+            }
+            return completion(true, nil)
+        }
+    }
+
+}
