d6f365b0ec5691da141ae48df9457211ecfaa0a5
h33h <beolki102@gmail.com>
2021-11-03 22:04:55 +0300

    Rename LoginFormViewController to SignInFormViewController Add implementation of signInButtonAction

--- Mars Rover/ViewControllers/SignInFormViewController.swift
+++ Mars Rover/ViewControllers/SignInFormViewController.swift
@@ -1,5 +1,5 @@
 //
-//  LoginFormViewController.swift
+//  SignInFormViewController.swift
 //  Mars Rover
 //
 //  Created by XXX on 2.11.21.
@@ -7,39 +7,53 @@
 
 import UIKit
 
-class LoginFormViewController: UIViewController {
+class SignInFormViewController: UIViewController {
 
-    // MARK: - LoginFormViewController: Variables
-    private var presenter: LoginFormPresenter?
+    // MARK: - SignInFormViewController: Variables
+    private var presenter: SignInFormPresenter?
 
-    // MARK: - LoginFormViewController: IBOutlet Variables
+    // MARK: - SignInFormViewController: IBOutlet Variables
     @IBOutlet private var emailTextField: AuthorizationTextField!
     @IBOutlet private var passwordTextField: AuthorizationTextField!
     @IBOutlet private var signInButton: UIButton!
     @IBOutlet private var createAccountButton: UIButton!
     @IBOutlet private var activityIndicator: UIActivityIndicatorView!
 
-    // MARK: - LoginFormViewController: Life Cycle Methods
+    // MARK: - SignInFormViewController: Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         emailTextField.delegate = self
         passwordTextField.delegate = self
-        presenter = LoginFormPresenter()
+        presenter = SignInFormPresenter()
         presenter?.setDelegate(delegate: self)
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         // when view appear check for login status
-        presenter?.login()
+        presenter?.signIn(signInType: .checkSignIn)
         setElementsDisabled(value: true)
     }
 
-}
+    // MARK: - SignInFormViewController: IBAction Methods
+    @IBAction private func signInButtonAction(_ sender: Any) {
+        guard let email = emailTextField.text,
+              let password = passwordTextField.text,
+              !email.isEmpty,
+              !password.isEmpty
+        else {
+            showSimpleNotificationAlert(title: "Sign In Failure", description: "Don't leave textfields empty")
+            return
+        }
+        presenter?.signIn(signInType: .emailAndPassword(email: email, password: password))
+        setElementsDisabled(value: true)
+    }
 
-extension LoginFormViewController {
+    @IBAction private func createAccountButtonAction(_ sender: Any) {
+        // TODO: go to signUP VC
+    }
 
-    // MARK: - LoginFormViewController: Methods
+    // MARK: - SignInFormViewController: Methods
     private func setElementsDisabled(value: Bool) {
         emailTextField.isEnabled = !value
         passwordTextField.isEnabled = !value
@@ -50,23 +64,10 @@ extension LoginFormViewController {
 
 }
 
-extension LoginFormViewController {
+extension SignInFormViewController: SignInFormPresenterDelegate {
 
-    // MARK: - LoginFormViewController: IBAction Methods
-    @IBAction private func signInButtonAction(_ sender: Any) {
-        presenter?.login()
-    }
-
-    @IBAction private func createAccountButtonAction(_ sender: Any) {
-        // TODO: go to signUP VC
-    }
-
-}
-
-extension LoginFormViewController: LoginFormPresenterDelegate {
-
-    // MARK: - LoginFormViewController: LoginFormPresenterDelegate Methods
-    func successfullLogin() {
+    // MARK: - SignInFormViewController: SignInFormPresenterDelegate Methods
+    func successfullSignIn() {
         // storyboard that contains MainMenuViewController
         let mainMenuStoryboard = UIStoryboard(name: "MainMenu", bundle: nil)
         guard let mainMenuVC = mainMenuStoryboard.instantiateViewController(withIdentifier: "MainMenu") as? MainMenuViewController
@@ -75,15 +76,18 @@ extension LoginFormViewController: LoginFormPresenterDelegate {
         present(mainMenuVC, animated: true, completion: nil)
     }
 
-    func failureLogin(error: String) {
+    func failureSignIn(error: String) {
+        if !error.isEmpty {
+            showSimpleNotificationAlert(title: "Sign In Failure", description: error)
+        }
         setElementsDisabled(value: false)
     }
 
 }
 
-extension LoginFormViewController: UITextFieldDelegate {
+extension SignInFormViewController: UITextFieldDelegate {
 
-    // MARK: - LoginFormViewController: UITextField Delegate Methods
+    // MARK: - SignInFormViewController: UITextField Delegate Methods
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // when click on return button, keyboard closes
         textField.resignFirstResponder()
