a636dcf2b13522d625cdccc3475cbe1cdb4c99a4
h33h <beolki102@gmail.com>
2021-11-06 21:15:44 +0300

    It's value observation class

---
+++ Mars Rover/Utility/Box.swift
@@ -0,0 +1,25 @@
+//
+//  Box.swift
+//  Mars Rover
+//
+//  Created by XXX on 6.11.21.
+//
+
+import Foundation
+
+final class Box<T> {
+  typealias Listener = (T) -> Void
+  var listener: Listener?
+  var value: T {
+    didSet {
+      listener?(value)
+    }
+  }
+  init(_ value: T) {
+    self.value = value
+  }
+  func bind(listener: Listener?) {
+    self.listener = listener
+    listener?(value)
+  }
+}
