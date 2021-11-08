43eb81a8c753aeb663dec548de6143e9a1b1bdfb
h33h <beolki102@gmail.com>
2021-11-08 22:17:39 +0300

    Add mainMenu implementation and viewModel

--- Mars Rover/ViewControllers/MainMenuViewController.swift
+++ Mars Rover/ViewControllers/MainMenuViewController.swift
@@ -8,7 +8,77 @@
 import UIKit
 
 class MainMenuViewController: UIViewController, Storyboarded {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
+  // MARK: - MainMenuViewController: Variables
+    private var viewModel = MainMenuViewModel()
+    weak var coordinator: MainMenuCoordinator?
+
+  // MARK: - MainMenuViewController: IBOutlet Variables
+    @IBOutlet private var usernameLabel: UILabel!
+
+  // MARK: - MainMenuViewController: Life Cycle Methods
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      viewModel.getProfile()
+      viewModel.isSignedOut.bind { [weak self] isSignedOut in
+        if self?.navigationController?.viewControllers.last === self {
+          if isSignedOut {
+            self?.signOutAction()
+          }
+        }
+      }
+      viewModel.signOutError.bind { [weak self] signOutError in
+        if self?.navigationController?.viewControllers.last === self {
+          self?.signOutError(error: signOutError)
+        }
+      }
+      viewModel.profileFetched.bind { [weak self] profileFetched in
+        if self?.navigationController?.viewControllers.last === self {
+          self?.profileGet(profile: profileFetched)
+        }
+      }
+      viewModel.fetchError.bind { [weak self] fetchError in
+        if self?.navigationController?.viewControllers.last === self {
+          self?.profileGetFailure(error: fetchError)
+        }
+      }
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+      super.viewDidDisappear(animated)
+      coordinator?.goBack()
+    }
+
+  // MARK: - MainMenuViewController: IBAction Methods
+    @IBAction private func signOutButton(_ sender: Any) {
+      viewModel.signOut()
+    }
+}
+
+extension MainMenuViewController {
+  // MARK: - MainMenuViewController: Methods
+    func signOutAction() {
+      viewModel.isSignedOut.value = false
+      coordinator?.goBack()
+      self.navigationController?.popViewController(animated: true)
+    }
+
+    func signOutError(error: String) {
+      if !error.isEmpty {
+        showSimpleNotificationAlert(title: "Sign Out Error", description: error)
+      }
+    }
+
+    func profileGet(profile: ProfileModel) {
+      DispatchQueue.main.async { [weak self] in
+        self?.usernameLabel.text = profile.username
+      }
+    }
+
+    func profileGetFailure(error: String) {
+      if error == "Profile not exist" {
+        viewModel.setupNewUser()
+      } else if !error.isEmpty {
+        showSimpleNotificationAlert(title: "Profile Get Error", description: error)
+      }
+    }
 }
---
+++ Mars Rover/ViewModel/MainMenuViewModel.swift
@@ -0,0 +1,78 @@
+//
+//  MainMenuViewModel.swift
+//  Mars Rover
+//
+//  Created by XXX on 8.11.21.
+//
+
+import Foundation
+
+class MainMenuViewModel {
+  // MARK: - MainMenuViewModel: Variables
+    private var authService = FirebaseAuthService(
+      signInService: FirebaseSignInService(),
+      signUpService: FirebaseSignUpService()
+    )
+    private var profileService = FirebaseProfileService(
+      profileFetchService: FirebaseProfileFetchService(),
+      profileWriteService: FirebaseProfileWriteService()
+    )
+    var fetchError = Box("")
+    var profileFetched = Box(ProfileModel(username: "Loading..."))
+    var signOutError = Box("")
+    var isSignedOut = Box(false)
+
+  // MARK: - MainMenuViewModel: Methods
+    func getProfile() {
+      profileService.profileFetchService.fetch { [weak self] profile, error in
+        guard let strongSelf = self else { return }
+          if let error = error {
+            switch error {
+            case .error(error: let error):
+              strongSelf.fetchError.value = error
+            case .notSignedIn:
+              strongSelf.fetchError.value = "Authentication error"
+            case .profileNotExist:
+              strongSelf.fetchError.value = "Profile not exist"
+            }
+            return
+          }
+          guard let profile = profile else {
+            strongSelf.fetchError.value = "Unknown error"
+              return
+          }
+          strongSelf.profileFetched.value = profile
+      }
+    }
+
+  func setupNewUser() {
+    profileService.profileWriteService.profileAction(action: .setupNewProfile) { [weak self] isCreated, error in
+      guard let strongSelf = self else { return }
+      if let error = error {
+        switch error {
+        case .error(let error):
+          strongSelf.fetchError.value = error
+        case .profileNotExist:
+          strongSelf.fetchError.value = "Profile not exist"
+        case .notSignedIn:
+          strongSelf.fetchError.value = "Authentication error"
+        }
+        return
+      }
+      if isCreated {
+        self?.getProfile()
+      }
+    }
+  }
+
+    func signOut() {
+      authService.signOut { [weak self] error in
+        guard let strongSelf = self else { return }
+          if let error = error {
+            strongSelf.signOutError.value = error.localizedDescription
+              return
+          }
+        strongSelf.isSignedOut.value = true
+      }
+    }
+}
