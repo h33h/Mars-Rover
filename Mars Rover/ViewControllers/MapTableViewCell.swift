e4ab82959981d1d8ce521df2a833b1964770001e
h33h <beolki102@gmail.com>
2021-11-15 15:29:29 +0300

    Add MapTableViewCell

---
+++ Mars Rover/ViewControllers/MapTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MapTableViewCell.swift
+//  Mars Rover
+//
+//  Created by XXX on 10.11.21.
+//
+
+import UIKit
+
+class MapTableViewCell: UITableViewCell {
+  @IBOutlet var mapLabel: UILabel!
+  @IBOutlet var mapLastEditLabel: UILabel!
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+}
