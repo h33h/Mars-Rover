1394d54e2902a7ea72ae79896836ccca5cefdbc3
h33h <beolki102@gmail.com>
2021-11-18 19:44:38 +0300

    Update ObstacleModel

--- Mars Rover/Model/Obstacle.swift
+++ Mars Rover/Model/Obstacle.swift
@@ -6,11 +6,32 @@
 //
 
 import Foundation
+import SceneKit
 
 enum Obstacle: Int, CaseIterable {
-  case none = 0
-  case solidGround = 1
-  case sand = 2
-  case pit = 3
-  case hill = 4
+  case solidGround = 0
+  case sand
+  case pit
+  case hill
+
+  func getBlock() -> SCNNode? {
+    switch self {
+    case .solidGround:
+      return SCNScene(named: "GameAssets.scnassets/solidBlock.scn")?.rootNode.childNode(withName: "Cube", recursively: true)
+    case .sand:
+      return SCNScene(named: "GameAssets.scnassets/sandBlock.scn")?.rootNode.childNode(withName: "Cube", recursively: true)
+    case .pit:
+      return SCNScene(named: "GameAssets.scnassets/blockWithHole.scn")?.rootNode.childNode(withName: "Cube", recursively: true)
+    case .hill:
+      return SCNScene(named: "GameAssets.scnassets/blockWithHill.scn")?.rootNode.childNode(withName: "Cube", recursively: true)
+    }
+  }
+
+  static func getObstacle(node: SCNNode) -> Obstacle? {
+    if node.name == "solidBlock" { return .solidGround }
+    if node.name == "sandBlock" { return .sand }
+    if node.name == "blockWithHole" { return .pit }
+    if node.name == "blockWithHill" { return .hill }
+    return nil
+  }
 }
