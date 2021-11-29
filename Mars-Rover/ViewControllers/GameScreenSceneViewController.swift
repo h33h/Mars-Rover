a76ba3a10e288b170772c4a72c52f9ffe7966b28
h33h <beolki102@gmail.com>
2021-11-29 18:11:48 +0300

    Add GameSceneVC and GameScreenViewModel for visualisation of map completion

---
+++ Mars-Rover/ViewControllers/GameScreenSceneViewController.swift
@@ -0,0 +1,76 @@
+//
+//  GameScreenSceneViewController.swift
+//  Mars Rover
+//
+//  Created by XXX on 24.11.21.
+//
+
+import SceneKit
+
+class GameScreenSceneViewController: UIViewController, Storyboarded {
+  var coordinator: BackFlow?
+  private var viewModel: GameScreenSceneViewModel?
+  private var scnScene: SCNScene?
+  @IBOutlet private var scnView: SCNView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupScene()
+    clearNodes()
+    setupMap()
+    setupMarsRover()
+  }
+
+  @IBAction private func playAgainAction(_ sender: Any) {
+    viewModel?.completeMap()
+  }
+
+  @IBAction private func exitAction(_ sender: Any) {
+    coordinator?.goBack()
+  }
+
+  func setViewModel(viewModel: GameScreenSceneViewModel) {
+    self.viewModel = viewModel
+  }
+
+  private func setupScene() {
+    guard let scnScene = SCNScene(named: "mapScene.scn", inDirectory: "art.scnassets") else { return }
+    scnView.scene = scnScene
+    scnView.showsStatistics = true
+    self.scnScene = scnScene
+  }
+
+  private func setupMap() {
+    guard
+      let scnScene = scnScene,
+      let mapNode = scnScene.rootNode.childNode(withName: "board", recursively: true)
+    else { return }
+    if let mapManagerNode = viewModel?.mapCreator.mapNode {
+      mapNode.addChildNode(mapManagerNode)
+    }
+  }
+
+  private func setupMarsRover() {
+    guard
+      let scnScene = scnScene,
+      let roverNode = scnScene.rootNode.childNode(withName: "rover", recursively: true)
+    else { return }
+    if let roverManagerNode = viewModel?.roverManager.marsRover {
+      roverNode.addChildNode(roverManagerNode)
+    }
+  }
+
+  private func clearNodes() {
+    guard
+      let scnScene = scnScene,
+      let mapNode = scnScene.rootNode.childNode(withName: "board", recursively: true),
+      let roverNode = scnScene.rootNode.childNode(withName: "rover", recursively: true)
+    else { return }
+    mapNode.enumerateChildNodes { node, _ in
+      node.removeFromParentNode()
+    }
+    roverNode.enumerateChildNodes { node, _ in
+      node.removeFromParentNode()
+    }
+  }
+}
